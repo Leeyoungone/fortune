b5aa01900347df11fbf891ca3f3e8102ccffdc82
Youngone Lee <youngone.lee@accenture.com>
2025-04-02 00:27:02 -0400

    trying to get the blocks to align horizontally

--- chat-client/app/Fortunes.js
+++ chat-client/app/Fortunes.js
@@ -1,7 +1,7 @@
 //Send Fortune 
 //See Received Fortune
 //two buttons that redirect or open a separate "room"
-import { Text } from 'react-native'; 
+import { StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native'; 
 import { useRouter } from 'expo-router';
 import { Dimensions } from 'react-native';
 
@@ -10,9 +10,49 @@ const boundedHeight = Dimensions.get('window').height;
 const Fortunes = () => {
     const router = useRouter();
 
+    const receivedFortune = () => {
+        router.push('/Received');
+    }; 
+
+    const sendFortune = () => {
+        router.push('/Send');
+    }
+
     return (
-        <Text> Some Text Here </Text>
+        <View> 
+            <Text> Some Text Here </Text>
+            <TouchableOpacity onPress={receivedFortune}> 
+                <Text style={styles.formButtonLabel}> RECEIVED</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={sendFortune}> 
+                <Text style={styles.formButtonLabel}> SEND</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
+
+
+
+const styles = StyleSheet.create({
+    formButtonLabel: {
+        ...Platform.select({
+            android: {
+                fontSize: 16,
+                paddingTop: 12
+            },
+            ios: {
+                fontSize: 16,
+                paddingTop: 12
+            },
+            default: {
+                fontSize: 24,
+                paddingTop: 20,
+                display: 'inline-block', 
+                backgroundColor: '#DDDDDD', 
+            }
+        })
+    }
+});
 
 export default Fortunes; 
